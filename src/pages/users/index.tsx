d7df6def02c572633bf7a8179f3a0f948d241bc9
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-11 19:13:20 -0300

    Component: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidbar';
 
 export default function UsersList() {
@@ -132,6 +133,7 @@ export default function UsersList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
