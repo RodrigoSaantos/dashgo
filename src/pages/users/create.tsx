58c3e084578045960f4624a9439e296c78fa7c3d
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-13 22:28:31 -0300

    Sidebar responsive

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
-import { Sidebar } from '../../components/Sidbar';
+import { Sidebar } from '../../components/Sidebar';
 
 export default function CreateUser() {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,7 +16,7 @@ import {
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
-import { Sidebar } from '../../components/Sidbar';
+import { Sidebar } from '../../components/Sidebar';
 
 export default function UsersList() {
   return (
