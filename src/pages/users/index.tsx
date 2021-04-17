c84c23a4cbcb05b36dcd0a9276f3550b267ee2c5
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-17 00:10:24 -0300

    Config React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -17,21 +18,23 @@ import {
 import Link from 'next/link';
 import { useEffect } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UsersList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const responseData = await response.json();
+
+    return responseData;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, []);
 
   return (
     <Box>
@@ -55,110 +58,136 @@ export default function UsersList() {
               </Button>
             </Link>
           </Flex>
-
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" w={8}>
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w={[8]} p={0} />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Rodrigo Santos</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      contato.rodrigosaantos@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple">
-                    {isWideVersion ? (
-                      <>
-                        <Text as="span" marginInlineEnd={2}>
-                          <Icon as={RiPencilLine} fontSize={16} />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" w={8}>
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    <Th>Data de cadastro</Th>
+                    <Th w={[8]} p={0} />
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Rodrigo Santos</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          contato.rodrigosaantos@gmail.com
                         </Text>
-                        Editar
-                      </>
-                    ) : (
-                      <Icon as={RiPencilLine} fontSize={16} />
-                    )}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Rodrigo Santos</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      contato.rodrigosaantos@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple">
-                    {isWideVersion ? (
-                      <>
-                        <Text as="span" marginInlineEnd={2}>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                    <Td>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                      >
+                        {isWideVersion ? (
+                          <>
+                            <Text as="span" marginInlineEnd={2}>
+                              <Icon as={RiPencilLine} fontSize={16} />
+                            </Text>
+                            Editar
+                          </>
+                        ) : (
                           <Icon as={RiPencilLine} fontSize={16} />
+                        )}
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Rodrigo Santos</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          contato.rodrigosaantos@gmail.com
                         </Text>
-                        Editar
-                      </>
-                    ) : (
-                      <Icon as={RiPencilLine} fontSize={16} />
-                    )}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Rodrigo Santos</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      contato.rodrigosaantos@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple">
-                    {isWideVersion ? (
-                      <>
-                        <Text as="span" marginInlineEnd={2}>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                    <Td>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                      >
+                        {isWideVersion ? (
+                          <>
+                            <Text as="span" marginInlineEnd={2}>
+                              <Icon as={RiPencilLine} fontSize={16} />
+                            </Text>
+                            Editar
+                          </>
+                        ) : (
                           <Icon as={RiPencilLine} fontSize={16} />
+                        )}
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Rodrigo Santos</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          contato.rodrigosaantos@gmail.com
                         </Text>
-                        Editar
-                      </>
-                    ) : (
-                      <Icon as={RiPencilLine} fontSize={16} />
-                    )}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                    <Td>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                      >
+                        {isWideVersion ? (
+                          <>
+                            <Text as="span" marginInlineEnd={2}>
+                              <Icon as={RiPencilLine} fontSize={16} />
+                            </Text>
+                            Editar
+                          </>
+                        ) : (
+                          <Icon as={RiPencilLine} fontSize={16} />
+                        )}
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
