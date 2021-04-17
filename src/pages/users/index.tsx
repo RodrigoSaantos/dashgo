13b9286752caaca79e0889021172162be605d54f
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-17 12:53:46 -0300

    Creating list hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,32 +22,10 @@ import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import { api } from '../../services/api';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UsersList() {
-  const { data, isLoading, isFetching, error } = useQuery(
-    'users',
-    async () => {
-      const responseData = await api.get('users');
-
-      const users = responseData.data.users.map(user => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pr-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    },
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
