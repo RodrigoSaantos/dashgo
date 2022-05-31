59e444f8f37f445f42662733028bbbcf70d7a9b6
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-30 22:56:01 -0300

    Component: Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from 'next'
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import { Input } from '../components/Form/Input';
 
 const Home: NextPage = () => {
   return (
@@ -19,39 +20,8 @@ const Home: NextPage = () => {
         flexDir="column"
       >
         <Stack spacing={4} >
-          <FormControl>
-            <FormLabel htmlFor="email" >E-mail</FormLabel>
-            <Input
-              name="email"
-              id="email"
-              type="email"
-              focusBorderColor="pink.800"
-              bg="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor:
-                  "gray.900",
-              }}
-              size="lg"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password" >Password</FormLabel>
-            <Input
-              name="password"
-              id="password"
-              type="password"
-              focusBorderColor="pink.800"
-              bg="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor:
-                  "gray.900"
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
         </Stack>
 
         <Button
