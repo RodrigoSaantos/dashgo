09ed1fd4d18fccad9896d77093233222c4b360a8
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-06-01 09:54:55 -0300

    Responsive registration

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
-import { Sidebar } from '../../components/Sidbar';
+import { Sidebar } from '../../components/Sidebar';
 
 export default function CreateUser() {
   return (
@@ -18,7 +18,7 @@ export default function CreateUser() {
       <Header />
       <Flex w="100%" my={6} maxW={1480} mx="auto" px={6}>
         <Sidebar />
-        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+        <Box flex={1} borderRadius={8} bg="gray.800" p={[6, 8]}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -26,12 +26,12 @@ export default function CreateUser() {
           <Divider my={6} borderColor="gray.700" />
 
           <VStack spacing={8}>
-            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+            <SimpleGrid minChildWidth={240} spacing={[6, 8]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+            <SimpleGrid minChildWidth={240} spacing={[6, 8]} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_confirmation"
